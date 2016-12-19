ae86c7e34c3b378c69127c863a8cdd88b78e921e
sunshineyanghui <1187073986@qq.com>
2016-12-19 16:55:58 +0800

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { Router, Route, browserHistory, Link } from 'react-router';
 class App extends React.Component{
   render(){
     return(
       <div>
-        hello world
+        <div>
+          <Link to='/hello1'>Hello1</Link>
+          <Link to='/hello2'>Hello2</Link>
+        </div>
       </div>
     )
   }
