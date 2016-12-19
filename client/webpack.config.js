fd0c8e86aaff61d8cfa7049069054b322fc06dc4
sunshineyanghui <1187073986@qq.com>
2016-12-19 15:57:39 +0800

    react hello

---
+++ client/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+// path 是 nodejs 自带的一个包，所以不用安装，直接使用
+// 用来完成*路径*相关操作
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
