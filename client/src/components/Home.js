49c56a9a0c28cb76f2fb055c30d06016daac20ff
sunshineyanghui <1187073986@qq.com>
2016-12-26 11:29:38 +0800

    delete post

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import {Link} from 'react-router';
+import filter from 'lodash/fp/filter';
 class Home extends React.Component{
   constructor(){
     super();
@@ -12,13 +13,21 @@ class Home extends React.Component{
     axios.get('http://localhost:3000/posts')
     .then( res => this.setState({data:res.data.posts}))
   }
+  handleClick(id){
+    console.log('handleClick'+id);
+    // filter(this.state.data,post.id)
+    const posts = filter((post) => {
+      return post._id !== id
+    }, this.state.data);
+    this.setState({ data: posts });
+  }
   render(){
     let postList = this.state.data.map( (post,i) => (
       <div　key={i}　className="post-card">
         <Link to={`/posts/${post._id}`}>{post.title}</Link>
         <div　style={{float:'right'}}>
           <Link to={`/edit/${post._id}`}>修改</Link>
-          <Link to={`/posts/${post._id}`}>删除</Link>
+          <span className="delete-btn" onClick={this.handleClick.bind(this,post._id)}>删除</span>
         </div>
       </div>
 
